5389fc28eeb4d09d3305058c25e9b2b5c52aefc0
evesalazar516 <evesalazar516@gmail.com>
2023-09-02 19:12:40 -0500

    eve

--- htdocs/mysql.js
+++ htdocs/mysql.js
@@ -1,3 +1,8 @@
+
+const express = requiere('express')
+const app = express ();
+
+
 const mysql = require('mysql')
 
 const connection = mysql.createConnection ({
@@ -10,8 +15,33 @@ database:'act4'
 
 connection.connect ((err) =>{
 
-    if(err) throw err
+  if(err) throw err
 
     console.log('Conexión Exitosa')
+})
+
+app.set("view engine","ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.get("/", function (req,res){
+    res.render("act2.ejs");
+});
+app.post("/validar", function (req,res){
+  const datos =req.body;
+  console.log(datos);
+  let correo = datos.correo;
+  let nombre = datos.nombre;
+  let contraseña = datos.contraseña;
+  let telefono =  datos.telefono;
+
+  let registrar = "INSERT INTO  info_usuarios (correo, nombre, contraseña, telefono) VALEUS ('"+correo +"', '"+nombre +"', '"+contraseña +"', '"+telefono +"')";
+  connection.query(act2.ejs)
+});
+
+
+
+app.listen(3000, function(){
+console.log("servidor creado http://localhost:3000");
 })
 
