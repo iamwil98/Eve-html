61c15cb8d4701b808f2f5e6c276fccca2aaa9e0a
iamwil98 <92549065+iamwil98@users.noreply.github.com>
2023-09-02 19:09:57 -0500

    rama segunda

--- htdocs/mysql.js
+++ htdocs/mysql.js
@@ -1,17 +1,43 @@
-const mysql = require('mysql')
+const path = require('path');
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 
-const connection = mysql.createConnection ({
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'actividad4',
+    password: 'actividad42023**',
+    database: 'act4'
+});
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname));
 
-host:'localhost',
-user:'actividad4',
-password:'actividad42023**',
-database:'act4'
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-connection.connect ((err) =>{
+app.get("/", function(req, res) {
+    res.render("act2");
+});
 
-    if(err) throw err
+app.post("/validar", function(req, res) {
+    const { correo, nombre, password, telefono } = req.body;
 
-    console.log('Conexión Exitosa')
-})
+    const registrar = "INSERT INTO info_usuarios (correo, nombre, contraseña, telefono) VALUES (?, ?, ?, ?)";
+    
+    pool.query(registrar, [correo, nombre, password, telefono], (err, result) => {
+        if (err) {
+            console.error("Error al insertar: ", err);
+            res.status(500).send("Error al registrar usuario");
+            return;
+        }
+        
+        // Puedes responder con un mensaje o redireccionar, por ejemplo:
+        res.send("Usuario registrado con éxito!");
+    });
+});
 
+app.listen(3000, function() {
+    console.log("Servidor escuchando en http://localhost:3000");
+});
